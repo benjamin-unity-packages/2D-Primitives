35be9eb9d6d747d23c47df57e48a91fe270f6fe8
Benjamin Fischer <benjamin.fischer.unity@gmail.com>
2021-05-09 18:18:07 +0200

    Added namespace

--- Runtime/Primitives2D.cs
+++ Runtime/Primitives2D.cs
@@ -2,54 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Primitives2D
-{
-    private static GameObject Instantiate(GameObject prefab, Vector2 at, Color color)
+namespace BenjiDev {
+    public static class Primitives2D
     {
-        GameObject instance = Object.Instantiate(prefab);
-        instance.transform.position = at;
-        instance.GetComponent<SpriteRenderer>().material.color = color;
-        return instance;
-    }
+        private static GameObject Instantiate(GameObject prefab, Vector2 at, Color color)
+        {
+            GameObject instance = Object.Instantiate(prefab);
+            instance.transform.position = at;
+            instance.GetComponent<SpriteRenderer>().material.color = color;
+            return instance;
+        }
 
-    public static GameObject Capsule(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("Capsule"), at, color);
-    }
+        public static GameObject Capsule(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("Capsule"), at, color);
+        }
 
-    public static GameObject Character(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("Character"), at, color);
-    }
+        public static GameObject Character(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("Character"), at, color);
+        }
 
-    public static GameObject Circle(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("Circle"), at, color);
-    }
+        public static GameObject Circle(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("Circle"), at, color);
+        }
 
-    public static GameObject Diamond(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("Diamond"), at, color);
-    }
+        public static GameObject Diamond(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("Diamond"), at, color);
+        }
 
-    public static GameObject HexagonFlatTop(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("HexagonFlatTop"), at, color);
-    }
+        public static GameObject HexagonFlatTop(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("HexagonFlatTop"), at, color);
+        }
 
-    public static GameObject HexagonPointTop(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("HexagonPointTop"), at, color);
+        public static GameObject HexagonPointTop(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("HexagonPointTop"), at, color);
 
-    }
+        }
 
-    public static GameObject Square(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("Square"), at, color);
-    }
+        public static GameObject Square(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("Square"), at, color);
+        }
 
-    public static GameObject SquareRounded(Vector2 at, Color color)
-    {
-        return Instantiate(Resources.Load<GameObject>("SquareRounded"), at, color);
+        public static GameObject SquareRounded(Vector2 at, Color color)
+        {
+            return Instantiate(Resources.Load<GameObject>("SquareRounded"), at, color);
+        }
     }
 }
